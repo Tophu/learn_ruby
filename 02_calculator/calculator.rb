3a7e7816628331ccd52e0c29d3251f152a0a5a0d
Tophu <fissionmailed.arsenal@gmail.com>
2019-01-09 18:27:29 +0000

    Main Exercise Complete

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def sum(num)
+    num.sum
+end
+
+#Main Excercise COMPLETE
+
+
+=begin
+BONUS Exercise Below:
+- Multiply
+- Power
+- Factorial
+=end
+
+def multiply(x, y)
+    x * y
+end
+
+def power(x, y)
+    x ** y
+end
+
+def factorial()
+
+end
